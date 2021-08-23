0228db9d303a8c443cbdbdb62d4dbc40b380d55e
JAMorello <juan.ag.morello@gmail.com>
2021-08-23 17:59:44 -0300

    Update Menu and Button components

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
 import { VStack, Divider } from "@chakra-ui/react";
-import { useHistory } from "react-router-dom";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { RiTeamFill } from "react-icons/ri";
 import MenuButton from "./MenuButton";
 
 const Menu = () => {
-  const [currentPage, setCurrentPage] = useState("home");
-  let history = useHistory();
-
-  function handleClick(pageString) {
-    setCurrentPage(pageString);
-    history.push(`/${pageString}`);
-  }
-
   return (
     <VStack
       borderWidth="2px"
@@ -26,17 +16,13 @@ const Menu = () => {
       <MenuButton
         text="Meet our workers"
         icon={<BsFillPersonLinesFill />}
-        page="meet-people"
-        currentPage={currentPage}
-        onClick={handleClick}
+        page="/meet-people"
       />
       <Divider />
       <MenuButton
         text="Ensemble a team"
         icon={<RiTeamFill />}
-        page="ensemble-team"
-        currentPage={currentPage}
-        onClick={handleClick}
+        page="/ensemble-team"
       />
     </VStack>
   );
--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,14 +1,22 @@
 import { Button } from "@chakra-ui/react";
+import { useLocation, useHistory } from "react-router-dom";
 
-const MenuButton = ({ text, icon, page, currentPage, onClick }) => {
+const MenuButton = ({ text, icon, page }) => {
+  let location = useLocation();
+  let history = useHistory();
+
+  function handleClick(pageString) {
+    history.push(pageString);
+  }
+
   return (
     <Button
       w="full"
       leftIcon={icon}
-      colorScheme={currentPage === page ? "orange" : "blue"}
-      isActive={currentPage === page}
+      colorScheme={location.pathname === page ? "orange" : "blue"}
+      isActive={location.pathname === page}
       variant="outline"
-      onClick={() => onClick(page)}
+      onClick={() => handleClick(page)}
     >
       {text}
     </Button>
