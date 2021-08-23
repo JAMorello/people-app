7f04638de52e588c524194855adb1d8fcfb26584
JAMorello <juan.ag.morello@gmail.com>
2021-08-23 17:03:18 -0300

    Update component styling

--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -3,28 +3,32 @@ import {
   AlertIcon,
   AlertTitle,
   AlertDescription,
+  Flex,
 } from "@chakra-ui/react";
 
 const AlertMessage = () => {
   return (
-    <Alert
-      status="error"
-      variant="subtle"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      textAlign="center"
-      height="200px"
-    >
-      <AlertIcon boxSize="40px" mr={0} />
-      <AlertTitle mt={4} mb={1} fontSize="lg">
-        Cannot retrieve data!
-      </AlertTitle>
-      <AlertDescription maxWidth="sm">
-        There is a problem while trying to retrieve data from the API. Our devs
-        are working on it. Meanwhile, go take a cooffe.
-      </AlertDescription>
-    </Alert>
+    <Flex justifyContent="center" alignItems="center" h="100%">
+      <Alert
+        status="error"
+        variant="subtle"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        textAlign="center"
+        height="200px"
+        w="50%"
+      >
+        <AlertIcon boxSize="40px" mr={0} />
+        <AlertTitle mt={4} mb={1} fontSize="lg">
+          Cannot retrieve data!
+        </AlertTitle>
+        <AlertDescription maxWidth="sm">
+          There is a problem while trying to retrieve data from the API. Our
+          devs are working on it. Meanwhile, go take a cooffe.
+        </AlertDescription>
+      </Alert>
+    </Flex>
   );
 };
 
